81807da3c321a2972bbcbfdbff1195b45883ee70
Richard Iribarren <richardiribarren@gmail.com>
2023-03-14 16:58:30 -0400

    fix: update app component to standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  imports: [CommonModule, RouterModule],
 })
 export class AppComponent {
   title = 'angular-bootstrap-standalone';
